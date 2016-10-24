5e531a5aebe4fa04c95f312a82057308e983b308
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-10-24 03:26:47 -0200

    feat: this is pretty much everything

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const TelegramBot = require('node-telegram-bot-api'),
 httpAgent.maxSockets = 15;
 var $;
 
-bot.answerInlineQuery()
-
 bot.onText(/\/receita (.+)/ , (msg, match) => {
   console.log(msg)
   let fromId = msg.chat.id;
@@ -22,7 +20,12 @@ bot.onText(/\/receita (.+)/ , (msg, match) => {
 
   request({uri: `${rootSite}/busca.php?q=${match[1]}`, pool: httpAgent}).then(html => {
     $ = cheerio.load(html);
-    let links = $('.box-hover > a').toArray();
+    let links = $('[class="box-hover"] > a').toArray().map(item => {
+      if ($(item).children('.photo-holder').children().get(0).attribs.src.match(/recipe_original/g))
+        $(item).remove();
+      else
+        return item;
+    });
     let linkEscolhido = Math.floor(Math.random() * links.length + 1)
     return request(`${rootSite}/${links[linkEscolhido].attribs.href}`);
   })
@@ -32,10 +35,12 @@ bot.onText(/\/receita (.+)/ , (msg, match) => {
     pageInfo.ingredients = $('.p-ingredient').toArray().map(item => $(item).text());
     pageInfo.instructions = $('.e-instructions > ol > li > span')
       .toArray().map(item => $(item).text());
-    pageInfo.photoUrl = $('.photo.pic.u-photo').get(0).attribs.src.split('?')[0];
+
+    if ($('.photo.pic.u-photo').length > 0)
+      pageInfo.photoUrl = $('.photo.pic.u-photo').get(0).attribs.src.split('?')[0];
+
     pageInfo.hash = crypto.createHash('md5').update(pageInfo.title).digest('hex');
 
-    console.log(pageInfo);
     return request({uri: pageInfo.photoUrl, encoding: 'binary'});
   })
   .then((data) => {
@@ -46,7 +51,5 @@ bot.onText(/\/receita (.+)/ , (msg, match) => {
     return bot.sendMessage(fromId, `Receita: ${pageInfo.title} \n*Ingredientes* \n\n${pageInfo.ingredients.join('\n\n')}` +
       `\n\n*Modo de preparo*\n${pageInfo.instructions.join('\n\n')}`, {parse_mode: 'Markdown'});
   })
-  .catch(error => {
-    console.log(error);
-  });
+  .catch(error => console.log(error));
 });
